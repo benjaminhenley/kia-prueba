bfc57fee5bb9cebcd8c6619c7bb810238da272d3
JoeTambor <joeareco@encendercomunicacion.com>
2025-04-07 16:12:35 -0300

    FIX - Agrega breakpoints pantallas window

--- src/Components/Inicio/HomeSlider.jsx
+++ src/Components/Inicio/HomeSlider.jsx
@@ -4,54 +4,63 @@ import { Link } from "react-router-dom";
 
 const HomeSlider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [fade, setFade] = useState(true); // Estado para controlar la opacidad
 
   // Auto rotation functionality
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % homeSlider.length);
-    }, 5000); // Cambia cada 5 segundos
+      setFade(false); // Inicia el fade out
+      setTimeout(() => {
+        setCurrentSlide((prev) => (prev + 1) % homeSlider.length);
+        setFade(true); // Inicia el fade in
+      }, 300); // Duración del fade out
+    }, 7000); // Cambia cada 5 segundos
 
     return () => clearInterval(interval);
   }, []);
 
   // Función para cambiar manualmente la diapositiva
   const goToSlide = (slideIndex) => {
-    setCurrentSlide(slideIndex);
+    setFade(false); // Inicia el fade out
+    setTimeout(() => {
+      setCurrentSlide(slideIndex);
+      setFade(true); // Inicia el fade in
+    }, 300); // Duración del fade out
   };
 
   const slide = homeSlider[currentSlide];
 
   return (
-    <div className="relative w-full h-[calc(100vh-1rem)] overflow-hidden">
+    <div className="relative w-full h-[calc(100dvh-1rem)] overflow-hidden">
       {/* Contenedor de imagen para móvil con proporción definida */}
       <div className="relative w-full h-full">
         {/* Imagen Mobile - Ahora con 100vh */}
         <img
           src={slide.imagenMobile}
           alt={slide.titulo}
-          className="block lg:hidden relative absolute w-full h-full object-cover transition-opacity duration-1000"
+          className={`block lg:hidden relative absolute w-full h-full object-cover transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}
         />
 
         {/* Imagen Desktop */}
         <img
           src={slide.imagen}
           alt={slide.titulo}
-          className="hidden lg:block w-full h-full object-cover transition-opacity duration-1000"
+          className={`hidden lg:block w-full h-full object-cover transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}
         />
         {/* Contenido - Ahora absolutamente posicionado tanto en móvil como en desktop */}
         <div className="absolute px-1 pb-3 inset-0 flex flex-col items-center justify-end text-white z-10">
           <h1
-            className="text-[2rem] xl:text-[2.25rem] 2xl:text-[4rem] font-bold  animate-fade-in"
+            className="text-[2rem] xl:text-[2.25rem] 2xl:text-[4rem] win:text-[2rem] font-bold  animate-fade-in"
             style={{ animation: "fadeIn 1s ease-in" }}>
             {slide.titulo}
           </h1>
-          <p className="text-[1rem] py-1 xl:text-[1.5rem] 2xl:text-[2.5rem] 2xl:py-3">{slide.subtitulo}</p>
+          <p className="text-[1rem] py-1 xl:text-[1.5rem] 2xl:text-[2.5rem] 2xl:py-3 win:text-[1rem] win:py-1">{slide.subtitulo}</p>
           {slide.esExterna ? (
             <a
               href={slide.linkBoton}
               target={slide.target}
               rel="noopener noreferrer"
-              className="group mt-5 mb-10 lg:mt-3 lg:mb-6 xl:mt-5 xl:mb-10 inline-block">
+              className="group mt-5 mb-10 lg:mt-3 lg:mb-6 xl:mt-5 xl:mb-10 win:mt-3 win:mb-5 inline-block">
               <div className="relative bg-white font-bold text-[0.875rem] text-midnight-black py-5 px-10 hover:bg-[#37434C] hover:text-white transition">
                 <span className="relative block">
                   {slide.textoBoton}
@@ -62,7 +71,7 @@ const HomeSlider = () => {
           ) : (
             <Link
               to={slide.linkBoton}
-              className="group mt-5 mb-10 lg:mt-3 lg:mb-6 xl:mt-5 xl:mb-10 inline-block">
+              className="group mt-5 mb-10 lg:mt-3 lg:mb-6 xl:mt-5 xl:mb-10 win:mt-3 win:mb-5 inline-block">
               <div className="relative bg-white font-bold text-[0.875rem] text-midnight-black py-5 px-10 hover:bg-[#37434C] hover:text-white transition">
                 <span className="relative block">
                   {slide.textoBoton}
