11883038cd4442900c943c7628ee2196e9062bcd
benjahenley <benjahenley@hotmail.com>
2025-05-09 12:18:10 -0300

    FIX - tabla perfeccionada

--- src/Components/Common/Table.jsx
+++ src/Components/Common/Table.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Table = ({ data, className }) => {
   return (
     <div className={`overflow-x-auto ${className}`}>
-      <table className="w-full">
+      <table className="w-full border border-[#CDD0D2]">
         {data.headers && (
           <thead className="bg-[#05141F]">
             <tr>
@@ -11,16 +11,17 @@ const Table = ({ data, className }) => {
                 <th
                   key={index}
                   width={header.width ? header.width : "100%"}
-                  className={`px-3 text-left sm:px-6 py-3 sm:py-4  text-white ${
+                  className={`px-3 text-left sm:px-6 py-3 sm:py-4 text-white ${
                     header.bold ? "font-bold" : ""
-                  }`}>
+                  } border-0`}
+                  style={{ borderBottom: "1px solid #CDD0D2" }}>
                   <h4 className="font-bold">{header.label}</h4>
                 </th>
               ))}
             </tr>
           </thead>
         )}
-        <tbody className="bg-white divide-y divide-x divide-[#CDD0D2]">
+        <tbody>
           {data.details.map((row, index) => (
             <tr
               key={index}
@@ -28,7 +29,7 @@ const Table = ({ data, className }) => {
               {row.map((cell, cellIndex) => (
                 <td
                   key={cellIndex}
-                  className="px-3 text-left sm:px-6 py-3 sm:py-4 text-sm text-[#05141F] border-r">
+                  className="px-3 text-left sm:px-6 py-3 sm:py-4 border border-t-2 border-[#CDD0D2] text-[#05141F]">
                   <h6
                     className={
                       data.headers[cellIndex]?.bold ? "font-bold" : "font-light"
