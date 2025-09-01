623b989d2002f56093c14f9cd4fa718c4a8e0bf0
benjahenley <benjahenley@hotmail.com>
2025-09-01 11:38:55 -0300

    FIX - SACO BANNER SANTANDER

--- src/Data/inicio.js
+++ src/Data/inicio.js
@@ -1,5 +1,4 @@
 // Imports fotos Desktop
-import SeltosSantander from "../assets/img/inicio/homeSlider/desktop/Seltos Santander.webp";
 import AllNewK3 from "../assets/img/inicio/homeSlider/desktop/All-new K3.webp";
 import AllNewK3Cross from "../assets/img/inicio/homeSlider/desktop/All-new K3 Cross.webp";
 import AllNewK3Sedan from "../assets/img/inicio/homeSlider/desktop/All-new K3 Sedan.webp";
@@ -9,7 +8,6 @@ import Seltos from "../assets/img/inicio/homeSlider/desktop/Seltos.webp";
 import K2500 from "../assets/img/inicio/homeSlider/desktop/K2500.webp";
 
 //Imports fotos Mobile
-import SeltosSantanderMobile from "../assets/img/inicio/homeSlider/mobile/Seltos Santander Mobile.webp";
 import AllNewK3Mobile from "../assets/img/inicio/homeSlider/mobile/All-new K3 Mobile.webp";
 import AllNewK3CrossMobile from "../assets/img/inicio/homeSlider/mobile/All-new K3 Cross Mobile.webp";
 import AllNewK3SedanMobile from "../assets/img/inicio/homeSlider/mobile/All-new K3 Sedan Mobile.webp";
@@ -20,16 +18,6 @@ import K2500Mobile from "../assets/img/inicio/homeSlider/mobile/K2500 Mobile.web
 
 // Este archivo contiene los datos para el slider
 export const homeSlider = [
-    {
-    imagen: SeltosSantander,
-    imagenMobile: SeltosSantanderMobile,
-    titulo: "",
-    subtitulo: "",
-    textoBoton: "Quiero mi Seltos",
-    linkBoton: "/red-venta",
-    target: "_blank",
-    esExterna: false,
-  },
   {
     imagen: AllNewK3,
     imagenMobile: AllNewK3Mobile,
