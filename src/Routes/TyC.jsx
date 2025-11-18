10f54abc2bfae601a0adc7cf1ec79db475ae0ac2
benjahenley <benjahenley@hotmail.com>
2025-11-18 09:45:26 -0300

    FIX - agrego nuevos TyC

--- src/Routes/TyC.jsx
+++ src/Routes/TyC.jsx
@@ -1,25 +1,147 @@
-import React from 'react'
+import React from "react";
 
 const TyC = () => {
   return (
-    <div className='bg-[#05141F] text-white p-6 min-h-screen'>
-      <h3 className='text-center text-2xl font-bold mt-[6rem] mb-6'>Términos y Condiciones</h3>
-      <section className='mb-8'>
-        <h4 className='text-lg font-semibold mb-2'>Kia Seltos – Tasa UVA 0% 12 MESES - Santander</h4>
-        <p className='text-sm mb-2'>Disponible para cartera de consumo. Válido para las operaciones solicitadas en el día de hoy en todo el territorio de la República Argentina. Stock 1000 préstamos. Otorgamiento sujeto a condiciones de contratación y evaluación crediticia. Las tasas informadas son a título meramente orientativo, pudiendo variar en función del monto y plazo de la operación, de la política de crédito de Santander y del comportamiento crediticio del solicitante. Las tasas aplicables en cada caso serán informadas al momento de solicitar el préstamo y antes de su otorgamiento. Costo financiero total efectivo anual:</p>
-        <p className='text-2xl font-bold mb-2'>CFTEA 27,62%</p>
-        <p className='text-xs mb-2'>(incluye seguro para un KIA SELTOS 1.5 LX CVT terceros completo en CABA) tasa fija nominal anual: 0,00%. Tasa efectiva nominal anual: 0,00%. Sistema de amortización francés. Monto mínimo: $2.000.000, monto máximo: $24.700.000. Plazo mínimo: 12 meses, plazo máximo: 12 meses.</p>
-        <p className='text-xs mb-2'>Ejemplo: para una financiación de $10.000.000 equivalente a 6337 UVAs (según el valor de la UVA al 09/09/2025) otorgada en CABA por 12 meses y TNA del 0,00%, la primera cuota será de $833.333,33 más $115.347,65 de seguro automotor. (*) Préstamo en pesos ajustable al Coeficiente de Estabilización de Referencia (CER). (**) Monto máximo aplicable a todos los concesionarios KIA del país exceptuando a Tierra del Fuego. (***) Gastos por cancelación del préstamo a cargo del cliente. <span className='italic'>Para más información y condiciones aplicables, consulte <a href='https://www.santander.com.ar' className='underline text-blue-400' target='_blank' rel='noopener noreferrer'>www.santander.com.ar</a>.</span></p>
+    <div className="bg-[#05141F] text-white p-6 min-h-screen">
+      <h3 className="text-center text-2xl font-bold mt-[6rem] mb-6">
+        Términos y Condiciones
+      </h3>
+      <section className="mb-8">
+        <h4 className="text-lg font-semibold mb-2">
+          SELTOS – TASA UVA 0% 12 MESES
+        </h4>
+        <p className="text-sm mb-2">
+          Disponible para cartera de consumo. Válido para las operaciones
+          solicitadas en el día de hoy en todo el territorio de la República
+          Argentina. Stock 1000 préstamos. Otorgamiento sujeto a condiciones de
+          contratación y evaluación crediticia. Las tasas informadas son a
+          título meramente orientativo, pudiendo variar en función del monto y
+          plazo de la operación, de la política de crédito de Santander y del
+          comportamiento crediticio del solicitante. Las tasas aplicables en
+          cada caso serán informadas al momento de solicitar el préstamo y antes
+          de su otorgamiento. Costo financiero total efectivo anual:
+        </p>
+        <p className="text-2xl font-bold mb-2">CFTEA 27,62%</p>
+        <p className="text-xs mb-2">
+          (incluye seguro para un KIA SELTOS 1.5 LX CVT terceros completo en
+          CABA) tasa fija nominal anual: 0,00%. Tasa efectiva nominal anual:
+          0,00%. Sistema de amortización francés. Monto mínimo: $2.000.000,
+          monto máximo: $24.700.000. Plazo mínimo: 12 meses, plazo máximo: 12
+          meses.
+        </p>
+        <p className="text-xs mb-2">
+          Ejemplo: para una financiación de $10.000.000 equivalente a 6083 UVAs
+          (según el valor de la UVA al 11/11/2025) otorgada en CABA por 12 meses
+          y TNA del 0,00%, la primera cuota será de $833.333,33 más $153.284,00
+          de seguro automotor. (*) Préstamo en pesos ajustable al Coeficiente de
+          Estabilización de Referencia (CER). (**) Monto máximo aplicable a
+          todos los concesionarios KIA del país exceptuando a Tierra del
+          Fuego. (***) Gastos por cancelación del préstamo a cargo del cliente.{" "}
+          <span className="italic">
+            Para más información y condiciones aplicables, consulte{" "}
+            <a
+              href="https://www.santander.com.ar"
+              className="underline text-blue-400"
+              target="_blank"
+              rel="noopener noreferrer">
+              www.santander.com.ar
+            </a>
+            .
+          </span>
+        </p>
+      </section>
+      <section className="mb-8">
+        <h4 className="text-lg font-semibold mb-2">
+          SELTOS – TASA UVA 0% 18 MESES
+        </h4>
+        <p className="text-sm mb-2">
+          Disponible para cartera de consumo. Válido para las operaciones
+          solicitadas en el día de hoy en todo el territorio de la República
+          Argentina. Stock 1000 préstamos. Otorgamiento sujeto a condiciones de
+          contratación y evaluación crediticia. Las tasas informadas son a
+          título meramente orientativo, pudiendo variar en función del monto y
+          plazo de la operación, de la política de crédito de Santander y del
+          comportamiento crediticio del solicitante. Las tasas aplicables en
+          cada caso serán informadas al momento de solicitar el préstamo y antes
+          de su otorgamiento. Costo financiero total efectivo anual:
+        </p>
+        <p className="text-2xl font-bold mb-2">CFTEA 27,81%</p>
+        <p className="text-xs mb-2">
+           (incluye seguro para un KIA SELTOS 1.5 LX CVT terceros completo en
+          CABA) tasa fija nominal anual: 0,00%. Tasa efectiva nominal anual:
+          0,00%. Sistema de amortización francés. Monto mínimo: $2.000.000,
+          monto máximo: $14.000.000. Plazo mínimo: 18 meses, plazo máximo: 18
+          meses.
+        </p>
+        <p className="text-xs mb-2">
+          Ejemplo: para una financiación de $10.000.000 equivalente a 6083 UVAs
+          (según el valor de la UVA al 11/11/2025) otorgada en CABA por 12 meses
+          y TNA del 0,00%, la primera cuota será de $833.333,33 más $153.284,00
+          de seguro automotor. (*) Préstamo en pesos ajustable al Coeficiente de
+          Estabilización de Referencia (CER). (**) Monto máximo aplicable a
+          todos los concesionarios KIA del país exceptuando a Tierra del
+          Fuego. (***) Gastos por cancelación del préstamo a cargo del cliente.{" "}
+          <span className="italic">
+            Para más información y condiciones aplicables, consulte{" "}
+            <a
+              href="https://www.santander.com.ar"
+              className="underline text-blue-400"
+              target="_blank"
+              rel="noopener noreferrer">
+              www.santander.com.ar
+            </a>
+            .
+          </span>
+        </p>
       </section>
       <section>
-        <h4 className='text-lg font-semibold mb-2'>Kia Seltos – Tasa UVA 0% 18 MESES - Santander</h4>
-        <p className='text-sm mb-2'>Disponible para cartera de consumo. Válido para las operaciones solicitadas en el día de hoy en todo el territorio de la República Argentina. Stock 1000 préstamos. Otorgamiento sujeto a condiciones de contratación y evaluación crediticia. Las tasas informadas son a título meramente orientativo, pudiendo variar en función del monto y plazo de la operación, de la política de crédito de Santander y del comportamiento crediticio del solicitante. Las tasas aplicables en cada caso serán informadas al momento de solicitar el préstamo y antes de su otorgamiento. Costo financiero total efectivo anual:</p>
-        <p className='text-2xl font-bold mb-2'>CFTEA 27,81%</p>
-        <p className='text-xs mb-2'>(incluye seguro para un KIA SELTOS 1.5 LX CVT terceros completo en CABA) tasa fija nominal anual: 0,00%. Tasa efectiva nominal anual: 0,00%. Sistema de amortización francés. Monto mínimo: $2.000.000, monto máximo: $14.000.000. Plazo mínimo: 18 meses, plazo máximo: 18 meses.</p>
-        <p className='text-xs mb-2'>Ejemplo: para una financiación de $10.000.000 equivalente a 6337 UVAs (según el valor de la UVA al 09/09/2025) otorgada en CABA por 18 meses y TNA del 0,00%, la primera cuota será de $555.555,56 más $115.347,65 de seguro automotor. (*) Préstamo en pesos ajustable al Coeficiente de Estabilización de Referencia (CER). (**) Monto máximo aplicable a todos los concesionarios KIA del país exceptuando a Tierra del Fuego. (***) Gastos por cancelación del préstamo a cargo del cliente. <span className='italic'>Para más información y condiciones aplicables, consulte <a href='https://www.santander.com.ar' className='underline text-blue-400' target='_blank' rel='noopener noreferrer'>www.santander.com.ar</a>.</span></p>
+        <h4 className="text-lg font-semibold mb-2">
+          K3 – TASA UVA 0% 12 y 18 MESES
+        </h4>
+        <p className="text-sm mb-2">
+          (1) Disponible para cartera de consumo. Válido para las operaciones
+          solicitadas en el día de hoy en todo el territorio de la República
+          Argentina. Stock 1000 préstamos. Otorgamiento sujeto a condiciones de
+          contratación y evaluación crediticia. Las tasas informadas son a
+          título meramente orientativo, pudiendo variar en función del monto y
+          plazo de la operación, de la política de crédito de Santander y del
+          comportamiento crediticio del solicitante. Las tasas aplicables en
+          cada caso serán informadas al momento de solicitar el préstamo y antes
+          de su otorgamiento. Costo financiero total efectivo anual:
+        </p>
+        <p className="text-2xl font-bold mb-2">
+          CFTEA mín. 27,81% y máx. 27,81%
+        </p>
+        <p className="text-xs mb-2">
+          (incluye seguro para un KIA K3 0km terceros completo en CABA) tasa
+          fija nominal anual: mínima 0,00% y máxima 0,00%. Tasa efectiva nominal
+          anual: mínima 0,00% y máxima 0,00%. Sistema de amortización francés.
+          Monto mínimo: $2.000.000, monto máximo: $20.000.000. Plazo mínimo: 12
+          meses, plazo máximo: 18 meses.
+        </p>
+        <p className="text-xs mb-2">
+          Ejemplo: para una financiación de $10.000.000 equivalente a 6083 UVAs
+          (según el valor de la UVA al 11/11/2025) otorgada en CABA por 12 meses
+          y TNA del 0,00%, la primera cuota será de $833.333,33 más $153.284,00
+          de seguro automotor. (*) Préstamo en pesos ajustable al Coeficiente de
+          Estabilización de Referencia (CER). (**) Monto máximo aplicable a
+          todos los concesionarios KIA del país exceptuando a Tierra del
+          Fuego. (***) Gastos por cancelación del préstamo a cargo del cliente.
+          <span className="italic">
+            Para más información y condiciones aplicables, consulte en{" "}
+            <a
+              href="https://www.santander.com.ar"
+              className="underline text-blue-400"
+              target="_blank"
+              rel="noopener noreferrer">
+              www.santander.com.ar
+            </a>
+            .
+          </span>
+        </p>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default TyC
+export default TyC;
